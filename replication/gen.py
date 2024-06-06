806618b07b82d3085fc872495cafb9f418944b13
Sumer Kohli <sumer.kohli@berkeley.edu>
2024-06-05 21:48:00 -0700

    Add replication scripts

---
+++ replication/gen.py
@@ -0,0 +1,142 @@
+import sys
+
+NUM_NODES = 32
+# QUEUE_ALGORITHM = "DropTail"
+# QUEUE_ALGORITHM = "RED"
+QUEUE_ALGORITHM = "CHOKe"
+
+MIN_TH = 25
+MAX_TH = 50
+
+UDP_ARRIVAL_RATE = f"{sys.argv[1]}Kb"
+
+simulation = """set ns [new Simulator]
+
+Queue/CHOKe set mean_pktsize_ 1000
+
+$ns color 1 Blue
+$ns color 2 Red
+
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+set tf [open out.tr w]
+$ns trace-all $tf
+
+
+proc finish {} {
+    global ns nf tf file1 file2
+    $ns flush-trace
+    close $nf
+    close $tf
+    close $file1
+    close $file2
+    exit 0
+}
+
+
+"""
+
+simulation += f"""
+Queue/CHOKe set thresh_ {MIN_TH}
+Queue/CHOKe set thresh_queue_ {MIN_TH}
+Queue/CHOKe set maxthresh_ {MAX_TH}
+Queue/CHOKe set maxthresh_queue_ {MAX_TH}
+"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"set tcp{i} [$ns node]\n"
+
+simulation += """
+set udp [$ns node]
+
+set router1 [$ns node]
+set router2 [$ns node]\n\n
+"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"set tcp_sink{i} [$ns node]\n"
+
+simulation += f"""\nset udp_sink1 [$ns node]
+$ns duplex-link $router1 $router2 1Mb 1ms {QUEUE_ALGORITHM}\n
+"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"$ns duplex-link $tcp{i} $router1 10Mb 1ms {QUEUE_ALGORITHM}\n"
+
+simulation += f"""\n$ns duplex-link $udp $router1 10Mb 1ms {QUEUE_ALGORITHM}\n\n"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"$ns duplex-link $tcp_sink{i} $router2 10Mb 1ms {QUEUE_ALGORITHM}\n"
+
+simulation += f"""\n$ns duplex-link $udp_sink1 $router2 10Mb 1ms {QUEUE_ALGORITHM}
+$ns queue-limit $router1 $router2 300
+
+set file2 [open queue.tr w]
+$ns trace-queue $router1 $router2 $file2\n\n"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"""set tcp_ag{i} [new Agent/TCP]
+$tcp_ag{i} set class_ 2
+$tcp_ag{i} set window_ 300
+$tcp_ag{i} set packetSize_ 1000
+$ns attach-agent $tcp{i} $tcp_ag{i}
+set sink{i} [new Agent/TCPSink]
+$ns attach-agent $tcp_sink{i} $sink{i}
+$ns connect $tcp_ag{i} $sink{i}
+$tcp_ag{i} set fid_ {i}\n\n"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"""set ftp{i} [new Application/FTP]
+$ftp{i} attach-agent $tcp_ag{i}
+$ftp{i} set type_ FTP\n\n"""
+
+simulation += f"""#Setup a UDP connection
+set udp_ag [new Agent/UDP]
+$ns attach-agent $udp $udp_ag
+set null [new Agent/Null]
+$ns attach-agent $udp_sink1 $null
+$ns connect $udp_ag $null
+$udp_ag set fid_ 33
+
+set file1 [open qm.out w]
+set qmon [$ns monitor-queue $router1 $router2 $file1 0.1]
+[$ns link $router1 $router2] queue-sample-timeout\n
+
+#Setup a CBR over UDP connection
+set cbr [new Application/Traffic/CBR]
+$cbr attach-agent $udp_ag
+$cbr set type_ CBR
+$cbr set packet_size_ 1000
+$cbr set rate_ {UDP_ARRIVAL_RATE}
+$cbr set random_ false\n\n"""
+
+simulation += """#Schedule events for the CBR and FTP agents
+$ns at 0.0 "$cbr start"
+"""
+
+for i in range(1, NUM_NODES + 1):
+    simulation += f"""$ns at 0.0 "$ftp{i} start"\n"""
+    simulation += f"""$ns at 120.0 "$ftp{i} stop"\n"""
+
+
+simulation += """
+$ns at 120.0 "$cbr stop"
+
+#Detach tcp and sink agents (not really necessary)
+# $ns at 4.5 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
+
+#Call the finish procedure after 5 seconds of simulation time
+$ns at 120.0 "finish"
+
+puts "here"
+
+$ns run\n"""
+
+
+with open(f"choke_{NUM_NODES}.tcl", "w") as f:
+    f.write(simulation)
+
+print(
+    f"Generated choke_{NUM_NODES}.tcl with {NUM_NODES} nodes and arrival rate of {UDP_ARRIVAL_RATE}"
+)
---
+++ replication/sweep.py
@@ -0,0 +1,145 @@
+import os
+import numpy as np
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def parse_queue_file(queue_file):
+    queue_size = defaultdict(int)
+    queue_totals = defaultdict(np.int64)
+    queue_nums = defaultdict(np.int64)
+    queue_enqueues = defaultdict(np.int64)
+    queue_drops = defaultdict(np.int64)
+
+    all_pkt_ids = defaultdict(set)
+    dropped_pkt_ids = defaultdict(set)
+
+    SAMPLE_INTERVAL = 0.1000
+    next_sample = SAMPLE_INTERVAL
+
+    with open(queue_file, "r") as fp:
+        for line in fp:
+            (
+                event,
+                time,
+                from_node,
+                to_node,
+                pkt_type,
+                pkt_size,
+                flags,
+                flow_id,
+                src_addr,
+                dst_addr,
+                seq_num,
+                pkt_id,
+            ) = line.split()
+
+            all_pkt_ids[pkt_type].add(pkt_id)
+
+            if event == "+":
+                queue_size[pkt_type] += 1
+            elif event in ("-", "d"):
+                queue_size[pkt_type] -= 1
+
+            if event == "d":
+                queue_drops[pkt_type] += 1
+                dropped_pkt_ids[pkt_type].add(pkt_id)
+
+            if event == "+":
+                queue_enqueues[pkt_type] += 1
+            # if event in ("-", "d"):
+
+            time = float(time)
+            if time >= next_sample:
+                next_sample += SAMPLE_INTERVAL
+                for key in queue_size:
+                    queue_nums[key] += 1
+                    queue_totals[key] += queue_size[key]
+
+            # if pkt_type == "cbr":
+            #     print(f"UDP packet @ t={time}")
+            # elif pkt_type == "tcp":
+            #     print(f"TCP packet @ t={time}")
+
+    queue_means = {}
+    for pkt_type in queue_totals:
+        queue_means[pkt_type] = np.float64(queue_totals[pkt_type]) / np.float64(
+            queue_nums[pkt_type]
+        )
+
+    print(f"Number of UDP time samples: {(queue_nums['cbr'])}")
+    print(f"Number of TCP time samples: {(queue_nums['tcp'])}")
+
+    print(
+        f"UDP drop rate: {(np.float64(len(dropped_pkt_ids['cbr'])) / np.float64(len(all_pkt_ids['cbr']))):%}"
+    )
+    print(
+        f"TCP drop rate: {(np.float64(len(dropped_pkt_ids['tcp'])) / np.float64(len(all_pkt_ids['tcp']))):%}"
+    )
+
+    print(
+        f"Total number of non-dropped UDP packets: {(len(all_pkt_ids['cbr']) - len(dropped_pkt_ids['cbr']))}"
+    )
+    print(
+        f"Total number of non-dropped TCP packets: {(len(all_pkt_ids['tcp']) - len(dropped_pkt_ids['tcp']))}"
+    )
+
+    print(f"Average UDP queue occupancy: {(queue_means['cbr'])}")
+    print(f"Average TCP queue occupancy: {(queue_means['tcp'])}")
+    print(
+        f"Average total queue occupancy: {(queue_means['cbr'] + (queue_means['tcp']))}"
+    )
+
+    return {
+        "avg_tcp": queue_means["tcp"],
+        "avg_udp": queue_means["cbr"],
+        "avg_total": queue_means["cbr"] + queue_means["tcp"],
+    }
+
+
+samples = np.logspace(2, 4, num=20)
+# samples = np.array([10**4])
+# samples = np.logspace(2, 5, num=25)
+print(samples)
+outputs = []
+for rate in samples:
+    print("\n\n")
+    print(f"Testing arrival rate: {np.int32(np.round(rate))}Kbps")
+    os.system(f"python gen.py {rate}")
+    os.system("rm *.out *.tr *.nam *.log 2>&1 > /dev/null 2>&1")
+    os.system("../ns choke_32.tcl > /dev/null")
+
+    occupancies = parse_queue_file("queue.tr")
+    outputs.append({"rate": rate, **occupancies})
+
+df = pd.DataFrame(outputs)
+
+# make seaborn plot look professional / academic, like a paper
+sns.set_theme(style="whitegrid")
+
+# nicer color scheme
+sns.set_palette("inferno", n_colors=3)
+
+# increase font size
+sns.set(font_scale=1.5)
+
+# plot all 3 lines in same plot
+sns.lineplot(x="rate", y="avg_tcp", data=df, markers=True, label="TCP Queue Occupancy")
+sns.lineplot(x="rate", y="avg_udp", data=df, markers=True, label="UDP Queue Occupancy")
+sns.lineplot(
+    x="rate", y="avg_total", data=df, markers=True, label="Total Queue Occupancy"
+)
+# add legend with custom labels
+plt.legend()
+
+# plot title and axis labels
+plt.title("Queue Occupancy vs. UDP Arrival Rate Under CHOKe")
+plt.title("Queue Occupancy vs. UDP Arrival Rate Under RED")
+plt.xlabel("UDP Arrival Rate (Kbps)")
+plt.ylabel("Queue Occupancy (packets)")
+# save figure please
+plt.xscale("log")
+plt.savefig("sweep.png")
+plt.show()
